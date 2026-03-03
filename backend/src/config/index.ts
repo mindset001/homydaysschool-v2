4985bcc630488d5ecf263c9426777db6d7a8e6f0
mindset001 <shittumoshoodademola@gmail.com>
2026-03-03 11:35:56 +0300

    'vercel'

--- backend/src/config/index.ts
+++ backend/src/config/index.ts
@@ -28,7 +28,7 @@ export const config = {
   cors: {
     origin: [
       process.env.FRONTEND_URL || 'http://localhost:5174',
-      'https://high-school-manager-zd2a.vercel.app',
+      'https://homydaysschool-v2.vercel.app/',
       'http://localhost:5173',
       'http://localhost:5174',
     ].filter(Boolean),
